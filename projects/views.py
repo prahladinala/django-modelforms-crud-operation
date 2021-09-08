662d4a52e840963075d0d421b07adf002e950998
Prahlad Inala <prahlad2018@gmail.com>
2021-09-08 16:17:58 +0530

    Taking POST request into views.py and print in console

--- projects/views.py
+++ projects/views.py
@@ -4,17 +4,26 @@ from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
 
+
 def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     # tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
+
 def createProject(request):
     form = ProjectForm()
+
+    if request.method == 'POST':
+        print(request.POST)
+        # print(request.POST['title'])
+        # form = ProjectForm(request.POST)
+
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
